98f9fe83473fbd1fef5992f77372ec2cd3945bb0
mrkyel <kyel100@naver.com>
2019-08-26 18:13:49 +0900

    Updates

---
+++ precache-manifest.067317011019f3d2ff5aaaef3bb1977d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23d349892feea4525c201a2f62c77c46",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "9a2c3c496e9b8f4ce799",
+    "url": "/movie_app_2019/static/css/main.ccfe9246.chunk.css"
+  },
+  {
+    "revision": "7c41f6268ddbd4a77207",
+    "url": "/movie_app_2019/static/js/2.70e103f6.chunk.js"
+  },
+  {
+    "revision": "9a2c3c496e9b8f4ce799",
+    "url": "/movie_app_2019/static/js/main.75bf5e4a.chunk.js"
+  },
+  {
+    "revision": "30a14d7c554eb6584568",
+    "url": "/movie_app_2019/static/js/runtime~main.353f3f61.js"
+  }
+]);
